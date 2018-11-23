a778ad906b3dc99593ee5e9f21883265f84efcf7
mathieu bouchant <matbcht@gmail.com>
2018-11-23 14:49:14 +0100

    cocktail v2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,3 +10,10 @@
 console.log('Hello World from Webpacker')
 
 import 'bootstrap';
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
