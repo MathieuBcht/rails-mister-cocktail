b3a46ea9b3809ef8d2a60c221a8760fe47912099
mathieu bouchant <matbcht@gmail.com>
2018-11-23 16:57:58 +0100

    v6 cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
-# Rails.application.routes.draw do
-#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-#   resources :cocktails, only: [ :index, :show, :new, :create ] do
-#     resources :doses, only: [ :new, :create ]
-#   end
-#   resources :doses, only: :destroy
-# end
-
 Rails.application.routes.draw do
-  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "cocktails#index"
-  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
-    resources :doses, only: [:new, :create]
-    resources :reviews, only: [:new, :create]
+  resources :cocktails, only: [ :index, :show, :new, :create ] do
+    resources :doses, only: [ :new, :create ]
   end
-  resources :doses, only: [:destroy]
+  resources :doses, only: :destroy
 end
+
+# Rails.application.routes.draw do
+#   # devise_for :users
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   root to: "cocktails#index"
+#   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
+#     resources :doses, only: [:new, :create]
+#     resources :reviews, only: [:new, :create]
+#   end
+#   resources :doses, only: [:destroy]
+# end
